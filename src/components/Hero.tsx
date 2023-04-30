3b55ca5a2ff69f6c6e8eae42257f07e1f48b6000
Tomas Coox <tomas@joox.se>
2023-04-30 23:57:33 +0200

    use clip-path package responsively

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,12 +2,13 @@ import Image from 'next/image'
 
 const HeroImage = () => {
     return (
-        <div className="w-full h-[230px] md:h-[400px] lg:h-[500px] xl:h-[650px] 2xl:h-[900px] relative">
+        <div className="w-full h-[400px] md:h-[600px] lg:h-[800px] xl:h-[800px] 2xl:h-[800px] relative">
             <Image
                 alt="Hero"
                 src="/Hast-profil-fran-vanster-smaller.jpg"
                 fill
-                style={{ objectFit: 'cover', clipPath: 'polygon(0 0, 100% 0, 100% 80%, 0 100%)' }}
+                style={{ objectFit: 'cover' }}
+                className="lg:clip-path-heroClip"
             />
             <div className='absolute w-full h-full flex items-center justify-center font-bebas-neue text-white font-bold text-4xl md:text-6xl lg:text-8xl xl:text-9xl'>
                 UPPLEVELSER I FJÄRRAN

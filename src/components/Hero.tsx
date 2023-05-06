fff5607638a2a524f86da255fe6edba05a85ec66
Tomas Coox <tomas@joox.se>
2023-05-06 20:33:28 +0200

    add subheading

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,16 +2,11 @@ import Image from 'next/image'
 
 const HeroImage = () => {
     return (
-        <div className="w-full h-[400px] md:h-[600px] lg:h-[800px] xl:h-[800px] 2xl:h-[800px] relative">
-            <Image
-                alt="Hero"
-                src="/Hast-profil-fran-vanster-smaller.jpg"
-                fill
-                style={{ objectFit: 'cover' }}
-                className="lg:clip-path-heroClip"
-            />
-            <div className='absolute w-full h-full flex items-center justify-center font-bebas-neue text-white text-3xl md:text-5xl lg:text-7xl xl:text-8xl'>
-                UPPLEVELSER I FJÄRRAN
+        <div className="w-full h-[250px] md:h-[600px] lg:h-[800px] xl:h-[800px] 2xl:h-[800px] relative">
+            <Image alt="Hero" src="/Hast-profil-fran-vanster-smaller.jpg" fill style={{ objectFit: 'cover' }} className="lg:clip-path-heroClip" />
+            <div className="absolute w-full h-full flex flex-col items-center justify-center text-white">
+                <div className='text-center font-bebas-neue text-5xl md:text-8xl lg:text-7xl xl:text-8xl'>UPPLEVELSER I FJÄRRAN</div>
+                <div className='text-center font-serif text-lg md:text-2xl lg:text-2xl xl:text-3xl'>Oförglömliga turer i den svenska fjällvärlden</div>
             </div>
         </div>
     )
